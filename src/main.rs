d3d00a6b4248dc8459e42ccf4812056bd9b02450
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-10-18 09:27:33 -0300

    References

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::stdin, process::exit, time::Instant};
+use std::{io::stdin, process::exit, rc::Rc, time::Instant};
 
 use serde::Deserialize;
 
@@ -8,8 +8,7 @@ macro_rules! binary_op {
             (Ok(Val::Bool(l)), Ok(Val::Bool(r))) => Ok(Val::Bool(l $op r)),
             (Err(e), _) | (_, Err(e)) => Err(e),
             _ => Err(CompilerError::new(
-                "Tipo invalido.".into(),
-                $term.location,
+                "Tipo invalido.".into()
             )),
         }
     }
@@ -21,8 +20,7 @@ macro_rules! binary_op_int {
             (Ok(Val::Int(l)), Ok(Val::Int(r))) => Ok(Val::Bool(l $op r)),
             (Err(e), _) | (_, Err(e)) => Err(e),
             _ => Err(CompilerError::new(
-                "Tipo invalido.".into(),
-                $term.location,
+                "Tipo invalido.".into()
             )),
         }
     }
@@ -33,22 +31,14 @@ struct Program {
     expression: Term,
 }
 
-#[derive(Debug, Deserialize, Clone)]
-struct Location {
-    start: usize,
-    end: usize,
-    filename: String,
-}
-
 #[derive(Debug, Deserialize)]
 struct CompilerError {
     message: String,
-    location: Location,
 }
 
 impl CompilerError {
-    fn new(message: String, location: Location) -> Self {
-        Self { message, location }
+    fn new(message: String) -> Self {
+        Self { message }
     }
 }
 
@@ -70,7 +60,6 @@ struct Bool {
 #[derive(Debug, Deserialize, Clone)]
 struct Print {
     value: Box<Term>,
-    location: Location,
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -78,7 +67,6 @@ struct Binary {
     lhs: Box<Term>,
     op: BinaryOp,
     rhs: Box<Term>,
-    location: Location,
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -121,20 +109,17 @@ struct If {
     condition: Box<Term>,
     then: Box<Term>,
     otherwise: Box<Term>,
-    location: Location,
 }
 
 #[derive(Debug, Deserialize, Clone)]
 struct Var {
     text: String,
-    location: Location,
 }
 
 #[derive(Debug, Deserialize, Clone)]
 struct Call {
     callee: Box<Term>,
     arguments: Vec<Term>,
-    location: Location,
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -155,7 +140,7 @@ enum Term {
 #[derive(Debug, Clone)]
 struct Func {
     parameters: Vec<Parameter>,
-    value: Box<Term>,
+    value: Rc<Term>,
 }
 
 #[derive(Clone, Debug)]
@@ -177,30 +162,25 @@ impl Runtime {
     }
 
     fn run(&mut self, program: Program) -> Result<Val, CompilerError> {
-        self.evaluate(program.expression)
+        self.evaluate(&program.expression)
     }
 
-    fn evaluate_print(&mut self, term: Print) -> Result<Val, CompilerError> {
-        let val = self.evaluate(*term.value);
+    fn evaluate_print(&mut self, term: &Print) -> Result<Val, CompilerError> {
+        let val = self.evaluate(&term.value);
 
         match val {
             Ok(Val::Str(val)) => println!("{val}"),
             Ok(Val::Int(val)) => println!("{val}"),
             Err(err) => return Err(err),
-            _ => {
-                return Err(CompilerError::new(
-                    "Tipo inválido para print".into(),
-                    term.location,
-                ))
-            }
+            _ => return Err(CompilerError::new("Tipo inválido para print".into())),
         };
 
         Ok(Val::Void)
     }
 
-    fn evaluate_binary(&mut self, term: Binary) -> Result<Val, CompilerError> {
-        let lhs = self.evaluate(*term.lhs);
-        let rhs = self.evaluate(*term.rhs);
+    fn evaluate_binary(&mut self, term: &Binary) -> Result<Val, CompilerError> {
+        let lhs = self.evaluate(&term.lhs);
+        let rhs = self.evaluate(&term.rhs);
 
         match term.op {
             BinaryOp::Add => match (lhs, rhs) {
@@ -209,17 +189,13 @@ impl Runtime {
                 (Ok(Val::Str(l)), Ok(Val::Int(r))) => Ok(Val::Str(l + &r.to_string())),
                 (Ok(Val::Int(l)), Ok(Val::Str(r))) => Ok(Val::Str(l.to_string() + &r)),
                 (Err(e), _) | (_, Err(e)) => Err(e),
-                _ => Err(CompilerError::new(
-                    "Operador inválido para soma.".into(),
-                    term.location,
-                )),
+                _ => Err(CompilerError::new("Operador inválido para soma.".into())),
             },
             BinaryOp::Sub => match (lhs, rhs) {
                 (Ok(Val::Int(l)), Ok(Val::Int(r))) => Ok(Val::Int(l - r)),
                 (Err(e), _) | (_, Err(e)) => Err(e),
                 _ => Err(CompilerError::new(
                     "Operador inválido para subtração.".into(),
-                    term.location,
                 )),
             },
             BinaryOp::Mul => match (lhs, rhs) {
@@ -227,33 +203,23 @@ impl Runtime {
                 (Err(e), _) | (_, Err(e)) => Err(e),
                 _ => Err(CompilerError::new(
                     "Operador inválido para subtração.".into(),
-                    term.location,
                 )),
             },
             BinaryOp::Rem => match (lhs, rhs) {
                 (Ok(Val::Int(l)), Ok(Val::Int(r))) => Ok(Val::Int(l % r)),
                 (Err(e), _) | (_, Err(e)) => Err(e),
-                _ => Err(CompilerError::new(
-                    "Operador inválido para resto.".into(),
-                    term.location,
-                )),
+                _ => Err(CompilerError::new("Operador inválido para resto.".into())),
             },
             BinaryOp::Div => match (lhs, rhs) {
                 (Ok(Val::Int(l)), Ok(Val::Int(r))) => {
                     if r == 0 {
-                        Err(CompilerError::new(
-                            "Divisão por zero é inválida.".into(),
-                            term.location,
-                        ))
+                        Err(CompilerError::new("Divisão por zero é inválida.".into()))
                     } else {
                         Ok(Val::Int(l / r))
                     }
                 }
                 (Err(e), _) | (_, Err(e)) => Err(e),
-                _ => Err(CompilerError::new(
-                    "Operador inválido para divisão.".into(),
-                    term.location,
-                )),
+                _ => Err(CompilerError::new("Operador inválido para divisão.".into())),
             },
 
             BinaryOp::Eq => match (lhs, rhs) {
@@ -261,20 +227,14 @@ impl Runtime {
                 (Ok(Val::Str(l)), Ok(Val::Str(r))) => Ok(Val::Bool(l == r)),
                 (Ok(Val::Bool(l)), Ok(Val::Bool(r))) => Ok(Val::Bool(l == r)),
                 (Err(e), _) | (_, Err(e)) => Err(e),
-                _ => Err(CompilerError::new(
-                    "Igualdade inválida.".into(),
-                    term.location,
-                )),
+                _ => Err(CompilerError::new("Igualdade inválida.".into())),
             },
             BinaryOp::Neq => match (lhs, rhs) {
                 (Ok(Val::Int(l)), Ok(Val::Int(r))) => Ok(Val::Bool(l != r)),
                 (Ok(Val::Str(l)), Ok(Val::Str(r))) => Ok(Val::Bool(l != r)),
                 (Ok(Val::Bool(l)), Ok(Val::Bool(r))) => Ok(Val::Bool(l != r)),
                 (Err(e), _) | (_, Err(e)) => Err(e),
-                _ => Err(CompilerError::new(
-                    "Não igualdade inválida.".into(),
-                    term.location,
-                )),
+                _ => Err(CompilerError::new("Não igualdade inválida.".into())),
             },
             BinaryOp::Lt => binary_op_int!(term, lhs < rhs),
             BinaryOp::Gt => binary_op_int!(term, lhs > rhs),
@@ -285,77 +245,65 @@ impl Runtime {
         }
     }
 
-    fn evaluate_let(&mut self, term: Let) -> Result<Val, CompilerError> {
-        let value = self.evaluate(*term.value)?;
-        self.env.push((term.name.text, value));
-        self.evaluate(*term.next)
+    fn evaluate_let(&mut self, term: &Let) -> Result<Val, CompilerError> {
+        let value = self.evaluate(&term.value)?;
+        self.env.push((term.name.text.clone(), value));
+        self.evaluate(&term.next)
     }
 
-    fn evaluate_function(&mut self, term: Function) -> Result<Val, CompilerError> {
+    fn evaluate_function(&mut self, term: &Function) -> Result<Val, CompilerError> {
         Ok(Val::Function(Func {
-            parameters: term.parameters,
-            value: term.value,
+            parameters: term.parameters.clone(),
+            value: Rc::new(*term.value.clone()),
         }))
     }
 
-    fn evaluate_if(&mut self, term: If) -> Result<Val, CompilerError> {
-        let condition = self.evaluate(*term.condition)?;
+    fn evaluate_if(&mut self, term: &If) -> Result<Val, CompilerError> {
+        let condition = self.evaluate(&term.condition)?;
 
         match condition {
-            Val::Bool(true) => self.evaluate(*term.then),
-            Val::Bool(false) => self.evaluate(*term.otherwise),
-            _ => Err(CompilerError::new(
-                "Condição inválida para if.".into(),
-                term.location,
-            )),
+            Val::Bool(true) => self.evaluate(&term.then),
+            Val::Bool(false) => self.evaluate(&term.otherwise),
+            _ => Err(CompilerError::new("Condição inválida para if.".into())),
         }
     }
 
-    fn evaluate_var(&mut self, term: Var) -> Result<Val, CompilerError> {
+    fn evaluate_var(&mut self, term: &Var) -> Result<Val, CompilerError> {
         for (name, value) in self.env.iter().rev() {
             if name == &term.text {
                 return Ok(value.clone());
             }
         }
 
-        Err(CompilerError::new(
-            "Variável não encontrada.".into(),
-            term.location,
-        ))
+        Err(CompilerError::new("Variável não encontrada.".into()))
     }
 
-    fn evaluate_call(&mut self, term: Call) -> Result<Val, CompilerError> {
-        if let Val::Function(func) = self.evaluate(*term.callee)? {
+    fn evaluate_call(&mut self, term: &Call) -> Result<Val, CompilerError> {
+        if let Val::Function(func) = self.evaluate(&term.callee)? {
             if term.arguments.len() != func.parameters.len() {
-                return Err(CompilerError::new(
-                    "Número de argumentos inválido.".into(),
-                    term.location,
-                ));
+                return Err(CompilerError::new("Número de argumentos inválido.".into()));
             }
 
             let original_stack_size = self.env.len();
 
-            for (parameter, argument) in func.parameters.into_iter().zip(term.arguments) {
+            for (parameter, argument) in func.parameters.iter().zip(term.arguments.iter()) {
                 let argument = self.evaluate(argument)?;
-                self.env.push((parameter.text, argument));
+                self.env.push((parameter.text.clone(), argument));
             }
 
-            let result = self.evaluate(*func.value);
+            let result = self.evaluate(&func.value);
             self.env.truncate(original_stack_size);
 
             result
         } else {
-            Err(CompilerError::new(
-                "Chamada inválida.".into(),
-                term.location,
-            ))
+            Err(CompilerError::new("Chamada inválida.".into()))
         }
     }
 
-    fn evaluate(&mut self, term: Term) -> Result<Val, CompilerError> {
+    fn evaluate(&mut self, term: &Term) -> Result<Val, CompilerError> {
         match term {
             Term::Int(term) => Ok(Val::Int(term.value)),
-            Term::Str(term) => Ok(Val::Str(term.value)),
+            Term::Str(term) => Ok(Val::Str(term.value.clone())),
             Term::Bool(term) => Ok(Val::Bool(term.value)),
             Term::Print(term) => self.evaluate_print(term),
             Term::Binary(term) => self.evaluate_binary(term),
@@ -376,10 +324,7 @@ fn main() {
     let instant = Instant::now();
 
     if let Err(e) = runtime.run(program) {
-        eprintln!(
-            "ERROR: {} {}:{}:{}",
-            e.message, e.location.filename, e.location.start, e.location.end
-        );
+        eprintln!("ERROR: {}", e.message);
         exit(1);
     }
 
